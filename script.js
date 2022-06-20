0abae79eaf04bb335d0a336db988eff953012711
matheus-teko <matheus.work94@gmail.com>
2022-06-20 20:11:17 -0300

    function

---
+++ script.js
@@ -0,0 +1,20 @@
+function msg (){
+    console.log ("TMJ!")
+}
+msg ()
+function myName(nome){
+console.log(`Bem-vinda, ${nome}!`)
+}
+myName("Matheus!")
+function aboutMe(name, age, music){
+console.log (`My name is ${name}, I have ${age} years and I love to listen ${music}`)
+}
+aboutMe("teko", 27, "RAP.")
+function fun(movie, music){
+    console.log (`My favorite film is ${movie}, and my favorite music is ${music}`)
+}
+    fun("BLADE RUNNER", "ARTIGO 157.")
+function calc (triplo){
+    return triplo * 3
+}
+ console.log (calc(5))
